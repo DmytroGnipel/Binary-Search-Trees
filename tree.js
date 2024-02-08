833d461d1fd2f6dc029b0bdca4d1fbc7b6fba556
Dmytro Hnipel <dmitrognipel@gmail.com>
2024-02-08 21:52:30 +0200

    Add function delete
    Added function delete, that created with the help while loop, not recursion

--- tree.js
+++ tree.js
@@ -1,41 +1,82 @@
 class Node {
-    constructor(head, leftChild = null, rightChild = null) {
-      this.head = head
-      this.leftChild = leftChild
-      this.rightChild = rightChild
+  constructor(head, leftChild = null, rightChild = null) {
+    this.head = head
+    this.leftChild = leftChild
+    this.rightChild = rightChild
+  }
+}
+
+class Tree {
+  #array
+  #processedArray
+  constructor(array) {
+    this.#array = array
+    this.#processedArray = this.#processArray(this.#array)
+    this.root = buildTree(this.#processedArray, 0, this.#processedArray.length - 1)
+  }
+    //get processed array - sorted and without duplicates
+  #processArray() {
+    const sorted = this.#array.sort((a, b) => a - b)
+    const withoutDuplicates = sorted.filter((elem, index, array) => {
+      return array.indexOf(elem) == index
+    })
+    return withoutDuplicates
+  }
+
+  insert(value, current = this.root) {
+    if (value < current.head) {
+      if (!current.leftChild) current.leftChild = new Node(value)
+      else {
+        this.insert(value, current.leftChild)
+      }
+    }
+    if (value > current.head) {
+      if (!current.rightChild) current.rightChild = new Node(value)
+      else {
+        this.insert(value, current.rightChild)
+      }
     }
   }
-  
-class Tree {
-    #array
-    #processedArray
-    constructor(array) {
-      this.#array = array
-      this.#processedArray = this.#processArray(this.#array)
-      this.root = buildTree(this.#processedArray, 0, this.#processedArray.length - 1)
+
+  delete(value) {
+    let parent = null
+    let current = this.root
+    while(current.head !== value) {//looking for the node with the given value
+      if (current.head > value) {
+      parent = current
+      current = current.leftChild
+      }
+      else if (current.head < value) {
+      parent = current
+      current = current.rightChild
+      }
     }
-    //get processed array - sorted and without duplicates
-    #processArray() {
-      const sorted = this.#array.sort((a, b) => a - b)
-      const withoutDuplicates = sorted.filter((elem, index, array) => {
-        return array.indexOf(elem) == index
-      })
-      return withoutDuplicates
+    //when the needed node is found
+    if (!current.rightChild && !current.leftChild)//when both children exist
+      current.head < parent.head ? parent.leftChild = null: parent.rightChild = null
+    else if (!current.rightChild) {//when only left child exists
+      current.head = current.leftChild.head//change the value of the current node
+      current.leftChild = null//...and remove node with the given value
     }
-    insert(value, current = this.root) {
-        if (value < current.head) {
-            if (!current.leftChild) current.leftChild = new Node(value)
-            else {
-                this.insert(value, current.leftChild)
-            }
-        }
-        if (value > current.head) {
-            if (!current.rightChild) current.rightChild = new Node(value)
-            else {
-                this.insert(value, current.rightChild)
-            }
-        }
+    else if (!current.leftChild) {//when only right child exists
+    current.head = current.rightChild.head
+    current.rightChild = null
     }
+    else {//when both children exist
+    current.head = findMin(current)//assign the min value of the right branch to current node
+    }//and unfairly remove the old node wit same value
+
+  function findMin (root) {
+    let previous = root
+    let current = root.rightChild
+    while (current.leftChild) {
+      previous = current
+      current = current.leftChild
+    }
+    previous.leftChild = null//delete node with min value
+    return current.head//return min value
+  }
+}
 }
 
   function buildTree(array, start, end) {
@@ -51,10 +92,15 @@ class Tree {
   return node
   }
   
-  const initialArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324, 34, 11, 2, 150]
+  const initialArray = [7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324, 34, 11, 2, 150]
   const tree = new Tree(initialArray)
-  tree.insert(6)
-  console.log(tree.root.leftChild)
+
+  tree.delete(67, tree.root)
+  
+  //console.log(tree.root.leftChild)
+
+  //console.log(tree.root.leftChild)
+ 
   
 //check with the function prettyPrint()
 const prettyPrint = (node, prefix = "", isLeft = true) => {
@@ -68,9 +114,15 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
     if (node.leftChild !== null) {
       prettyPrint(node.leftChild, `${prefix}${isLeft ? "    " : "│   "}`, true);
     }
-};
+}
 
 prettyPrint(tree.root)
+
+
+
+
+
+
   
   
   
